d4406343fedf1bcdb04370857a957b2fe83198e1
Un-knownCoder <34581283+Un-knownCoder@users.noreply.github.com>
2018-04-08 22:27:02 +0200

    Update tester.java

--- NEW/tester.java
+++ NEW/tester.java
@@ -1,16 +1,14 @@
-import UC.util.cartesianMath.geom2d.Polygon;
-import UC.util.Point2d;
+import UC.util.cartesianMath.geom3d.*; // (Sphere and Segment3d)
+import UC.util.Point3d;
 
 public class Main(){
   public static void main(String[] args){
-    Point2d p1 = new Point2d(10, 0);
-    Point2d p2 = new Point2d(10, 10);
-    Point2d p3 = new Point2d(0, 10);
-    Point2d p4 = new Point2d(0, 0);
+    Point3d A = new Point3d(10, 0);
+    Point3d B = new Point3d(10, 10);
     
-    Polygon p = new Polygon(p1, p2, p3, p4);
+    Segment3d s = new Segment3d(A, B);
+    Sphere sp = new Sphere(s);
     
-    System.out.println(p.getArea()); // the result should be 100
-    p.set(12, p1); // this should THROW an EXCEPTION called InvalidIndexException
+    System.out.println(sp.getVolume());
   }
 }
