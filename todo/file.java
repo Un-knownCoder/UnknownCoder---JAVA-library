691923e2bac67bac7a600e0a5ef2b33144f56b25
Un-knownCoder <34581283+Un-knownCoder@users.noreply.github.com>
2018-04-08 15:27:54 +0200

    Delete file.java

--- todo/file.java
+++
@@ -1,7 +0,0 @@
-private class TODO{
-  private static void list(){
-    String geom3d = "I need to find a formula to calculate the VOLUME of a generic 3D solid"; // TO DO
-    String exc = "I need create some EXCEPTIONS for my library (better representations of errors on the execution of a program)";  // Done
-    String polygon = "I need to fix the class polygon (the Polygon.area() method)"; // Done
-  }
-}
